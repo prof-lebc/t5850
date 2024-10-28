96f405531546ff28858ba753b5de793768e43849
prof-lebc <prof.lebc@gmail.com>
2024-10-28 13:44:39 -0300

    Add files via upload

---
+++ 28-10/atividades.js
@@ -0,0 +1,34 @@
+// 1. Função de Soma
+// Crie uma função chamada `soma` que recebe dois parâmetros `a` e `b`.
+// Dentro da função, declare uma variável chamada `resultado` que armazene a soma de `a` e `b`.
+// Retorne `resultado` e armazene-o dentro de uma variavel chamada `somaResultado`, e imprima-a no console.
+
+
+// 2. Concatenação de Strings
+// Escreva uma função chamada `concatenaNomes` que recebe dois parâmetros `nome` e `sobrenome`.
+// Retorne uma string que concatene `nome` e `sobrenome`, separando-os por um espaço e armazene-a em uma variavel chamada "nomeCompleto". Imprima-a no terminal.
+
+
+// 3. Maiúsculas
+// Crie uma função chamada `maiuscula` que recebe uma string `texto`.
+// Dentro da função, utilize o método `toUpperCase()` para converter `texto` em letras maiúsculas.
+// Imprima o resultado no console.
+
+
+// 4. Número de Caracteres
+// Escreva uma função chamada `contarCaracteres` que recebe uma string `texto`.
+// Declare uma variável chamada `quantidade` que armazene o comprimento da string usando `length`.
+// Retorne `quantidade` e armazene o retorno em uma variável de escopo global. Imprima esta variavel no terminal.
+
+
+// 5. Último Elemento de um Array
+// Crie uma função chamada `ultimoElemento` que recebe um array `array`.
+// Declare uma variável chamada `ultimo` que armazene o último elemento do array usando `array[array.length - 1]`.
+// Retorne `ultimo`.
+
+
+// 6. Remover Último Elemento de um Array
+// Escreva uma função chamada `removeUltimo` que recebe um array `array`.
+// Utilize o método `slice()` para retornar um novo array sem o último elemento.
+
+
