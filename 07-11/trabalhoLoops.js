a59c084db103ea1c64b47ae535face04ce9bb7f2
prof-lebc <prof.lebc@gmail.com>
2024-11-07 14:02:50 -0300

    Add files via upload

---
+++ 07-11/trabalhoLoops.js
@@ -0,0 +1,63 @@
+// 1. Crie uma função que receba um número e retorne a soma de todos os números de 1 até o número informado usando um laço `while`.
+
+
+// Exemplo de chamada:
+// console.log(somaAteNumero(5));  // 15 (1+2+3+4+5)
+
+// 2. Crie uma função que receba um array de números e retorne a soma de todos os números maiores que 10 usando um laço `for`.
+
+
+// Exemplo de chamada:
+// console.log(somaMaiorQueDez([5, 15, 3, 20, 7]));  // 35 (15 + 20)
+
+// 3. Crie uma função que, dado um array de filmes, retorne o nome do primeiro filme cujo título tem mais de 5 letras usando o `for...of`.
+
+
+// Exemplo de chamada:
+// console.log(filmeMaiorQueCincoLetras(["O Rei Leão", "Naruto", "Avengers", "Star Wars"]));  // "Avengers"
+
+// 4. Crie uma função que receba um array de nomes de personagens de *Hunter x Hunter* com os personagens "Gon Freecss", "Killua Zoldyck", "Leorio Paradinight" e um array de números representando a força de cada personagem, respectivamente 180, 195, 140. A função deve retornar um terceiro array com o nome dos personagens cuja força seja superior a 150, utilizando um laço `for`.
+
+
+// Exemplo de chamada:
+//const nomes = ["Gon Freecss", "Killua Zoldyck", "Leorio Paradinight"];
+//const forcas = [80, 95, 40];
+
+//console.log(personagemComForcaSuperiorA100(nomes, forcas));  
+
+
+// 5. Crie uma função que receba um array de números e retorne a quantidade de números negativos presentes no array, usando um laço `while`.
+
+
+// Exemplo de chamada:
+// console.log(contarNegativos([3, -2, -5, 7, 0, -1]));  // 3
+
+// 6. Crie uma função que receba uma string, converta-a para um array e retorne a quantidade de vezes que a letra "a" aparece nela.
+
+
+// Exemplo de chamada:
+// console.log(contarLetraA("Naruto é um anime top!"));  // 4
+
+// 7. Crie uma função que percorra um array de filmes e retorne um novo array com os filmes que têm o nome começando com a letra "S" usando o laço `for...of`.
+
+
+// Exemplo de chamada:
+// console.log(filmesComLetraS(["Star Wars", "Superman", "Shrek", "Batman"]));  // ["Star Wars", "Superman", "Shrek"]
+
+// 8. Crie uma função que receba um array de strings e retorne a maior substring (palavra) encontrada no array.
+
+
+// Exemplo de chamada:
+// console.log(maiorSubstring(["João", "Maria", "Alexandre", "Carlos"]));  // "Alexandre"
+
+// 9. Crie uma função que calcule o fatorial de um número utilizando um laço `for`.
+
+
+// Exemplo de chamada:
+// console.log(fatorial(5));  // 120 (5 * 4 * 3 * 2 * 1)
+
+// 10. Crie uma função que receba um número e retorne uma string que contém esse número repetido, separado por vírgulas, utilizando um laço `while`.
+
+
+// Exemplo de chamada:
+// console.log(repetirNumero(7, 4));  // "7, 7, 7, 7"
